22ee5dfa783d680b28ee07ea0d01ce32ee2f4a94
Julian Harris <julian.harris@gmail.com>
2023-12-21 11:33:10 +0000

    matrix row selection ability v1

--- inversion/matrix-component.js
+++ inversion/matrix-component.js
@@ -2,12 +2,13 @@ class MatrixComponent extends HTMLElement {
     static get observedAttributes() {
         return ['readonly', 'rows', 'cols']; // Observe readonly, rows, and cols attributes
     }
-
+ 
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
         this._matrix = this.initializeMatrix();
         this._readonly = false;
+        this.selectedRows = new Set(); // Keep track of selected rows
     }
 
     connectedCallback() {
@@ -17,8 +18,56 @@ class MatrixComponent extends HTMLElement {
         if (!this._readonly) {
             this.setupListeners();
         }
+        this.shadowRoot.addEventListener('click', this.onCellClick.bind(this));
     }
 
+    // Toggle row selection
+    toggleRowSelection(rowIndex) {
+        if (this.selectedRows.has(rowIndex)) {
+            this.selectedRows.delete(rowIndex);
+        } else {
+            this.selectedRows.add(rowIndex);
+        }
+        this.updateRowStyles();
+    }
+
+    updateRowStyles() {
+        const rows = this.shadowRoot.querySelectorAll('.matrix-row');
+        rows.forEach((row, index) => {
+            row.style.background = this.selectedRows.has(index) ? 'rgba(255, 255, 0, 0.2)' : 'none';
+        });
+    }
+    // Dispatch custom events for row selection/deselection
+    dispatchRowSelectionEvent(rowIndex, isSelected) {
+        const eventName = isSelected ? 'rowselected' : 'rowdeselected';
+        this.dispatchEvent(new CustomEvent(eventName, { detail: { rowIndex } }));
+    }
+    
+    // Method to externally select/deselect rows
+    selectRow(rowIndex) {
+        this.selectedRows.add(rowIndex);
+        this.updateRowStyles();
+    }
+
+    deselectRow(rowIndex) {
+        this.selectedRows.delete(rowIndex);
+        this.updateRowStyles();
+    }
+
+
+
+    onCellClick(event) {
+        const cell = event.target.closest('.matrix-cell');
+        if (cell) {
+            const row = event.target.closest('.matrix-row');
+            const rowIndex = Array.from(this.shadowRoot.querySelectorAll('.matrix-row')).indexOf(row);
+            if (rowIndex !== -1) {
+                this.toggleRowSelection(rowIndex);
+                this.dispatchRowSelectionEvent(rowIndex, this.selectedRows.has(rowIndex));
+            }
+        }
+    }
+    
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === 'readonly') {
             this.readonly = this.hasAttribute('readonly');
@@ -39,41 +88,48 @@ class MatrixComponent extends HTMLElement {
     render() {
         if (this._matrix && Array.isArray(this._matrix) && this._matrix.every(row => Array.isArray(row))) {
             
-            let content = this._matrix.map((row, i) => row.map((val, j) =>
+        let content = this._matrix.map((row, i) =>
+            `<div class="matrix-row">${row.map((val, j) =>
                 `<div class="matrix-cell">
                     ${this._readonly ? `<input id="m${i}${j}" value="${val}" readonly>` : `<input id="m${i}${j}" value="${val}">`}
                 </div>`
-            ).join('')).join('');
+            ).join('')}</div>`
+        ).join('');
+
 
             const cols = this._matrix[0].length;
             this.shadowRoot.innerHTML = `
                 <style>
                 .matrix-container {
-                    display: inline-flex;
-                    align-items: center;
-                    flex-wrap: nowrap;
-                    overflow: hidden;
+                    display: grid; /* Changed to grid for better control */
+                    grid-template-columns: auto 1fr auto; /* Brackets and matrix columns */
+                    grid-template-areas: "left-bracket matrix right-bracket";
+                    align-items: center; /* Center-align the children vertically */
+                    justify-items: center; /* Center-align the children horizontally */
+                    gap: 5px; /* Space between elements */
                     padding: 5px;
                     border: 1px solid lightgrey;
                     border-radius: 5px;
                     box-sizing: border-box;
-                    --bracket-size: 1em;
+                    --bracket-size: 1em; /* Maintain the bracket size variable */
                 }
                 .matrix {
+                    width: 100%; /* Set matrix width to fill the container */
                     display: grid;
-                    grid-template-columns: repeat(${cols}, auto);
-                    padding: 5px;
-                    gap: 2px
-                    box-sizing: border-box;
-                    margin-left: -15px;
-                    margin-right: -15px;
+                    grid-template-rows: repeat(auto-fill, minmax(min-content, max-content)); /* Create a new row for each .matrix-row */
+                    gap: 2px; /* Space between cells */
+                    grid-area: matrix; /* Assign to the center area */
                 }
                 .bracket {
-                    font-size: var(--bracket-size);
-                    font-weight: lighter
-                    user-select: none;
-                    padding: 0 2px;
                     margin-top: -15px;
+                    font-size: var(--bracket-size);
+                    font-weight: lighter;
+                    user-select: none;
+                    /* Removed padding to ensure brackets fit snugly */
+                    grid-area: left-bracket; /* Assign to the left area */
+                }
+                .bracket:last-child {
+                    grid-area: right-bracket; /* Assign to the right area */
                 }
                 input {
                     font-family: inherit;
@@ -86,11 +142,16 @@ class MatrixComponent extends HTMLElement {
                     padding: 0;
                     box-sizing: border-box;
                 }
+                .matrix-row {
+                    display: flex; /* Use flexbox for rows to layout cells horizontally */
+                    gap: 2px; /* Gap between cells */
+                }
+                
                 .matrix-cell {
-                    display: flex;
+                    flex: 1;
+                    display: flex; /* Flex to center the content of each cell */
                     justify-content: center;
                     align-items: center;
-                    margin: 0 2px;
                 }
                 </style>
                 <div class="matrix-container">
